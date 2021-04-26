5c554b9609f910a5eab1470d548446e26fe31ae7
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2021-04-26 17:57:03 +0530

    lol

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,49 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-void main() {
-  runApp(MyApp());
+import 'package:flutter/material.dart';
+import 'package:twitter_api/twitter_api.dart';
+import 'package:covidresource/key.dart';
+import 'package:tweet_ui/tweet_ui.dart';
+
+Future main() async {
+  final _twitterOauth = new twitterApi(
+      consumerKey: consumerApiKey,
+      consumerSecret: consumerApiSecret,
+      token: accessToken,
+      tokenSecret: accessTokenSecret);
+
+// Make the request to twitter
+  Future twitterRequest = _twitterOauth.getTwitterRequest(
+    // Http Method
+    "GET",
+    // Endpoint you are trying to reach
+    "statuses/user_timeline.json",
+    // The options for the request
+    options: {
+      "user_id": "19025957",
+      "screen_name": "TTCnotices",
+      "count": "20",
+      "trim_user": "true",
+      "tweet_mode": "extended", // Used to prevent truncating tweets
+    },
+  );
+
+  // Wait for the future to finish
+  var res = await twitterRequest;
+
+  // Print off the response
+  print(res.statusCode);
+  print(res.body);
+
+  // Convert the string response into something more useable
+  var tweets = json.decode(res.body);
+  print(tweets);
+  // runApp(MyApp());
 }
+
+// Creating the twitterApi Object with the secret and public keys
+// These keys are generated from the twitter developer page
+// Dont share the keys with anyone
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -22,92 +63,6 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
